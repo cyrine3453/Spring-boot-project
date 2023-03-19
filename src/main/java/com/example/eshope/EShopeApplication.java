eb774f730a1109be159b1fd5e7637c622348c314
9-barristanselmy-9 <71235080+9-barristanselmy-9@users.noreply.github.com>
2023-03-19 14:44:41 +0100

    first commit

---
+++ src/main/java/com/example/eshope/EShopeApplication.java
@@ -0,0 +1,21 @@
+package com.example.eshope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class EShopeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EShopeApplication.class, args);
+    }
+
+    @GetMapping("/Cyrine")
+    public  String hello(){
+        return "Cyrine Kaatttttousss";
+    }
+
+}
---
+++ src/test/java/com/example/eshope/EShopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eshope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EShopeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
